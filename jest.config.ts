841e3d7753eb2079c614c06e7ab414fbd3bf78fe
ohtaman <ohtamans@gmail.com>
2025-08-18 06:39:24 +0900

    Initial commit

---
+++ jest.config.ts
@@ -0,0 +1,20 @@
+import type { Config } from 'jest';
+
+const config: Config = {
+  preset: 'ts-jest',
+  testEnvironment: 'node',
+  testMatch: [
+    '**/__tests__/**/*.test.ts'
+  ],
+  moduleFileExtensions: [
+    'ts',
+    'js',
+    'json',
+    'node'
+  ],
+  moduleNameMapper: {
+    '^(\.{1,2}/.*)\.js$': '$1',
+  },
+};
+
+export default config;
---
+++ src/index.ts
@@ -0,0 +1,4 @@
+import { add } from './utils/math.js';
+
+
+console.log(add(3, 4));
